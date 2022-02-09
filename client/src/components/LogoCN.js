ce9627324e041019d709376b9e2c4a848a852c4b
agathecheri <82039936+agathecheri@users.noreply.github.com>
2022-02-09 17:52:29 +0100

    start and login screen modifications

--- client/src/components/LogoCN.js
+++ client/src/components/LogoCN.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Image, StyleSheet } from 'react-native'
 
-export default function Logo() {
-  return <Image source={require('../assets/cappuccino-1.png')} style={styles.image} />
+export default function LogoCN() {
+  return <Image source={require('../assets/LogoCN_Q.jpg')} style={styles.image} />
 }
 
 const styles = StyleSheet.create({
   image: {
-    width: 130,
+    width: 230,
     height: 130,
     marginBottom: 8,
   },
