421f4bb7d0e021277c3c930c3149b45cf75d9de7
Noé <noemasson@laposte.net>
2022-02-11 15:02:07 +0100

    First access to database to add a user

--- server/model/bdd.js
+++ server/model/bdd.js
@@ -2,26 +2,16 @@ const { Pool, Client } = require('pg')
 const fs = require('fs')
 
 
-try {
-
-    const data = fs.readFileSync('../properties/bdd.json', 'utf8');
-
-    // parse JSON string to JSON object
-    const logInfo = JSON.parse(data);
-
-} catch (err) {
-    console.log(`Error reading file from disk: ${err}`);
-}
-
-
+// Connect to database
+const data = fs.readFileSync('../properties/bdd.json', 'utf8');
+// parse JSON string to JSON object
+const logInfo = JSON.parse(data);
 const pool = new Pool(logInfo)
 
 
 
-
-
-const text = 'INSERT INTO users(name, email) VALUES($1, $2) RETURNING *'
-const values = ['brianc', 'brian.m.carlson@gmail.com']
+const text = 'INSERT INTO users(report_count, status) VALUES($1, $2)'
+const values = ['0', 'admin']
 
 
 // callback
@@ -33,27 +23,5 @@ pool.query(text, values, (err, res) => {
       // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
     }
   })
-  
-// ou bien
-const query = {
-    // give the query a unique name
-    name: 'fetch-user',
-    text: 'SELECT * FROM user WHERE id = $1',
-    values: [1],
-  }
 
-
-pool.query(query, (err, res) => {
-    if (err) {
-      console.log(err.stack)
-    } else {
-      console.log(res.rows[0])
-    }
-  })
-  
-
-pool.query('SELECT NOW()', (err, res) => {
-    console.log(err, res)
-    pool.end()
-  })
-
+pool.end()
