6c95cd2e3c8cfc8b22d9d29f880444c31b91473a
Noé <noemasson@laposte.net>
2022-03-04 16:32:43 +0100

    Updated database

--- server/database/nappuccino.sql
+++ server/database/nappuccino.sql
@@ -1,8 +1,20 @@
 
-CREATE SEQUENCE public.staff_staff_id_seq;
+CREATE TABLE status (
+                priviledge VARCHAR NOT NULL,
+                CONSTRAINT privilege PRIMARY KEY (priviledge)
+);
 
-CREATE TABLE public.staff (
-                staff_id INTEGER NOT NULL DEFAULT nextval('public.staff_staff_id_seq'),
+
+CREATE TABLE report_type (
+                type VARCHAR NOT NULL,
+                CONSTRAINT type PRIMARY KEY (type)
+);
+
+
+CREATE SEQUENCE staff_staff_id_seq;
+
+CREATE TABLE staff (
+                staff_id INTEGER NOT NULL DEFAULT nextval('staff_staff_id_seq'),
                 name VARCHAR NOT NULL,
                 first_name VARCHAR NOT NULL,
                 office VARCHAR,
@@ -12,60 +24,62 @@ CREATE TABLE public.staff (
 );
 
 
-ALTER SEQUENCE public.staff_staff_id_seq OWNED BY public.staff.staff_id;
+ALTER SEQUENCE staff_staff_id_seq OWNED BY staff.staff_id;
 
-CREATE SEQUENCE public.dispenser_dispenser_id_seq;
+CREATE SEQUENCE dispenser_dispenser_id_seq;
 
-CREATE TABLE public.dispenser (
-                dispenser_id INTEGER NOT NULL DEFAULT nextval('public.dispenser_dispenser_id_seq'),
+CREATE TABLE dispenser (
+                dispenser_id INTEGER NOT NULL DEFAULT nextval('dispenser_dispenser_id_seq'),
                 dispenser_type VARCHAR NOT NULL,
                 dispenser_status VARCHAR NOT NULL,
                 CONSTRAINT dispenser_id PRIMARY KEY (dispenser_id)
 );
 
 
-ALTER SEQUENCE public.dispenser_dispenser_id_seq OWNED BY public.dispenser.dispenser_id;
+ALTER SEQUENCE dispenser_dispenser_id_seq OWNED BY dispenser.dispenser_id;
 
-CREATE TABLE public.users (
+CREATE TABLE users (
                 login_ecn VARCHAR NOT NULL,
                 report_count INTEGER NOT NULL,
-                status VARCHAR,
+                priviledge VARCHAR NOT NULL,
                 CONSTRAINT user_id PRIMARY KEY (login_ecn)
 );
 
 
-CREATE SEQUENCE public.report_dispenser_report_dispenser_id_seq;
+CREATE SEQUENCE report_dispenser_report_dispenser_id_seq;
 
-CREATE TABLE public.report_dispenser (
-                report_dispenser_id INTEGER NOT NULL DEFAULT nextval('public.report_dispenser_report_dispenser_id_seq'),
+CREATE TABLE report_dispenser (
+                report_dispenser_id INTEGER NOT NULL DEFAULT nextval('report_dispenser_report_dispenser_id_seq'),
                 date TIMESTAMP NOT NULL,
-                report_type VARCHAR NOT NULL,
                 comment VARCHAR,
                 validation_count INTEGER NOT NULL,
                 dispenser_id INTEGER NOT NULL,
+                last_vote_date TIMESTAMP,
+                last_vote_type BOOLEAN,
                 login_ecn VARCHAR NOT NULL,
+                type VARCHAR NOT NULL,
                 CONSTRAINT report_dispenser_id PRIMARY KEY (report_dispenser_id)
 );
 
 
-ALTER SEQUENCE public.report_dispenser_report_dispenser_id_seq OWNED BY public.report_dispenser.report_dispenser_id;
+ALTER SEQUENCE report_dispenser_report_dispenser_id_seq OWNED BY report_dispenser.report_dispenser_id;
 
-CREATE SEQUENCE public.issues_issue_id_seq;
+CREATE SEQUENCE issues_issue_id_seq;
 
-CREATE TABLE public.issues (
-                issue_id INTEGER NOT NULL DEFAULT nextval('public.issues_issue_id_seq'),
+CREATE TABLE issues (
+                issue_id INTEGER NOT NULL DEFAULT nextval('issues_issue_id_seq'),
                 message VARCHAR NOT NULL,
                 login_ecn VARCHAR NOT NULL,
                 CONSTRAINT issue_id PRIMARY KEY (issue_id)
 );
 
 
-ALTER SEQUENCE public.issues_issue_id_seq OWNED BY public.issues.issue_id;
+ALTER SEQUENCE issues_issue_id_seq OWNED BY issues.issue_id;
 
-CREATE SEQUENCE public.report_ru_report_ru_id_seq;
+CREATE SEQUENCE report_ru_report_ru_id_seq;
 
-CREATE TABLE public.report_ru (
-                report_ru_id INTEGER NOT NULL DEFAULT nextval('public.report_ru_report_ru_id_seq'),
+CREATE TABLE report_ru (
+                report_ru_id INTEGER NOT NULL DEFAULT nextval('report_ru_report_ru_id_seq'),
                 date TIMESTAMP NOT NULL,
                 report_type VARCHAR NOT NULL,
                 comment VARCHAR,
@@ -75,32 +89,46 @@ CREATE TABLE public.report_ru (
 );
 
 
-ALTER SEQUENCE public.report_ru_report_ru_id_seq OWNED BY public.report_ru.report_ru_id;
+ALTER SEQUENCE report_ru_report_ru_id_seq OWNED BY report_ru.report_ru_id;
 
-ALTER TABLE public.report_dispenser ADD CONSTRAINT dispenser_report_dispenser_fk
+ALTER TABLE users ADD CONSTRAINT status_users_fk
+FOREIGN KEY (priviledge)
+REFERENCES status (priviledge)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE report_dispenser ADD CONSTRAINT report_type_report_dispenser_fk
+FOREIGN KEY (type)
+REFERENCES report_type (type)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE report_dispenser ADD CONSTRAINT dispenser_report_dispenser_fk
 FOREIGN KEY (dispenser_id)
-REFERENCES public.dispenser (dispenser_id)
+REFERENCES dispenser (dispenser_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE public.report_ru ADD CONSTRAINT users_report_ru_fk
+ALTER TABLE report_ru ADD CONSTRAINT users_report_ru_fk
 FOREIGN KEY (login_ecn)
-REFERENCES public.users (login_ecn)
+REFERENCES users (login_ecn)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE public.issues ADD CONSTRAINT users_issues_fk
+ALTER TABLE issues ADD CONSTRAINT users_issues_fk
 FOREIGN KEY (login_ecn)
-REFERENCES public.users (login_ecn)
+REFERENCES users (login_ecn)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE public.report_dispenser ADD CONSTRAINT users_report_dispenser_fk
+ALTER TABLE report_dispenser ADD CONSTRAINT users_report_dispenser_fk
 FOREIGN KEY (login_ecn)
-REFERENCES public.users (login_ecn)
+REFERENCES users (login_ecn)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
